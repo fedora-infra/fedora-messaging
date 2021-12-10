4ff9f7eee1ff639e31b0ce70ed8ad6bd191bd3cb
Aurélien Bompard <aurelien@bompard.org>
2021-12-10 16:32:19 +0100

    Update the get-authors.py script
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- news/get-authors.py
+++ news/get-authors.py
@@ -1,11 +1,50 @@
 #!/usr/bin/env python3
 
+"""
+This script browses through git commit history (starting at latest tag), collects all authors of
+commits and creates fragment for `towncrier`_ tool.
+
+It's meant to be run during the release process, before generating the release notes.
+
+Example::
+
+    $ python get_authors.py
+
+.. _towncrier: https://github.com/hawkowl/towncrier/
+
+Authors:
+    Aurelien Bompard
+    Michal Konecny
+"""
+
+import os
+from argparse import ArgumentParser
 from subprocess import check_output
 
 
-last_tag = check_output(["git", "describe", "--abbrev=0"], universal_newlines=True)
+EXCLUDE = ["Weblate (bot)"]
+
+last_tag = check_output(
+    "git tag | sort -n | tail -n 1", shell=True, universal_newlines=True
+).strip()
+
+args_parser = ArgumentParser()
+args_parser.add_argument(
+    "until",
+    nargs="?",
+    default="HEAD",
+    help="Consider all commits until this one (default: %(default)s).",
+)
+args_parser.add_argument(
+    "since",
+    nargs="?",
+    default=last_tag,
+    help="Consider all commits since this one (default: %(default)s).",
+)
+args = args_parser.parse_args()
+
 authors = {}
-log_range = last_tag.strip() + "..HEAD"
+log_range = args.since + ".." + args.until
 output = check_output(
     ["git", "log", log_range, "--format=%ae\t%an"], universal_newlines=True
 )
@@ -17,6 +56,12 @@ for line in output.splitlines():
     authors[email] = fullname
 
 for nick, fullname in authors.items():
-    with open(f"{nick}.author", "w") as f:
+    if fullname in EXCLUDE or fullname.endswith("[bot]"):
+        continue
+    filename = f"{nick}.author"
+    if os.path.exists(filename):
+        continue
+    print(f"Adding author {fullname} ({nick})")
+    with open(filename, "w") as f:
         f.write(fullname)
         f.write("\n")
