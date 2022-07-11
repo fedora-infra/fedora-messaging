434bcca0fe7cce0c1ab3377e454b44b2a35f8394
Aurélien Bompard <aurelien@bompard.org>
2022-07-11 11:44:35 +0200

    Fix docs configuration
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- docs/conf.py
+++ docs/conf.py
@@ -65,7 +65,7 @@ master_doc = "index"
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -186,5 +186,5 @@ intersphinx_mapping = {
     "pika": ("https://pika.readthedocs.io/en/latest/", None),
     "jsonschema": ("https://python-jsonschema.readthedocs.io/en/latest/", None),
     "blinker": ("https://pythonhosted.org/blinker/", None),
-    "Twisted": ("https://twistedmatrix.com/documents/current/api/", None),
+    "Twisted": ("https://docs.twisted.org/en/stable/api/", None),
 }
