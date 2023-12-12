92c2abdaf14a71afdfb0198918845b564428ac74
Aurélien Bompard <aurelien@bompard.org>
2023-12-12 09:50:59 +0100

    Use the common liccheck script
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- devel/run-liccheck.sh
+++ devel/run-liccheck.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+#
+# SPDX-FileCopyrightText: Contributors to the Fedora Project
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
 
-trap 'rm -f "$TMPFILE"' EXIT
+
+STRATEGY_URL=https://raw.githubusercontent.com/fedora-infra/shared/main/liccheck-strategy.ini
+
+trap 'rm -f "$TMPFILE $STRATEGY_TMPFILE"' EXIT
 
 set -e
+set -x
 
 TMPFILE=$(mktemp -t requirements-XXXXXX.txt)
+STRATEGY_TMPFILE=$(mktemp -t liccheck-strategy-XXXXXX.ini)
 
-poetry export --with dev -f requirements.txt --without-hashes -o $TMPFILE
+curl -o $STRATEGY_TMPFILE $STRATEGY_URL
 
-poetry run liccheck -r $TMPFILE
+poetry export --with dev --without-hashes -f requirements.txt -o $TMPFILE
+
+# Use pip freeze instead of poetry when it fails
+# poetry run pip freeze --exclude-editable --isolated > $TMPFILE
+
+poetry run liccheck -r $TMPFILE -s $STRATEGY_TMPFILE
