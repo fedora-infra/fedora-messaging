c21a2980bb99000a081789137c6fdf610c7536ba
Aurélien Bompard <aurelien@bompard.org>
2025-06-10 09:44:17 +0200

    Implement Statistics as dataclasses
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- tests/unit/twisted/test_stats.py
+++ tests/unit/twisted/test_stats.py
@@ -7,12 +7,6 @@ import pytest
 from fedora_messaging.twisted.stats import ConsumerStatistics
 
 
-def test_repr():
-    expected = "<ConsumerStatistics {'received': 0, 'processed': 0, 'dropped': 0, 'rejected': 0, 'failed': 0}>"
-    assert repr(ConsumerStatistics()) == expected
-    assert str(ConsumerStatistics()) == expected
-
-
 def test_stats_add():
     stats_1 = ConsumerStatistics()
     stats_1.received = 42
@@ -36,13 +30,4 @@ def test_stats_add_bad_type():
         ConsumerStatistics() + 42
     assert str(handler.value) == (
         "ConsumerStatistics instances can only be added to other ConsumerStatistics instances."
-    )
-
-
-def test_stats_bad_attr():
-    with pytest.raises(AttributeError) as handler:
-        ConsumerStatistics().dummy = 42
-    assert str(handler.value) == (
-        "ConsumerStatistics does not have a dummy attribute. Available attributes: dropped, "
-        "failed, processed, received, rejected."
     )
