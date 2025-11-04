fc45d73e6e952dc64d4efbb95b2b62be45991201
Jeremy Cline <jeremycline@linux.microsoft.com>
2025-11-04 16:21:08 -0500

    Install a new event loop explicitly
    Python introduced some breaking changes[0] which cause it to no longer
    implicitly create an event loop. Create a new one and install it
    explicitly so Twisted doesn't collapse at import.
    
    Note that Python plans to remove more APIs so this will certainly break
    in the future.
    
    [0] https://docs.python.org/3/whatsnew/3.14.html#id10
    
    Fixes: #531
    
    Signed-off-by: Jeremy Cline <jeremycline@linux.microsoft.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,12 +2,25 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+import asyncio
 import os
 
 import crochet
 import pytest
 
 from .utils import get_available_port
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_configure(config):
+    """
+    Short-term fix to support Python 3.14.
+
+    Twisted 25.5 does not support Python 3.14, and at the time of this writing neither
+    does pytest-twisted. This is because Python removed a number of APIs in asyncio. This
+    is a temporary workaround because they plan to remove these APIs too.
+    """
+    asyncio.set_event_loop(asyncio.new_event_loop())
 
 
 @pytest.fixture(autouse=True, scope="session")
